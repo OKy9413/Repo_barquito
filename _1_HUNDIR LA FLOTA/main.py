80a3eb8dcd1253dfcb5a7d7b2dfd394e1156991b
OKy9413 <jcrubgil@gmail.com>
2023-11-22 21:52:11 +0100

    up

--- _1_HUNDIR LA FLOTA/main.py
+++ _1_HUNDIR LA FLOTA/main.py
@@ -1,19 +1,22 @@
 import funciones as fun
 import variables as var
 
+fun.bienvenida()
+fun.suspense()
+fun.colocar_barcos()
+fun.colocar_barcos_maquina()
 while True:
-    fun.bienvenida()
     fun.mostrar_tableros()
     fun.contador_turnos()
     fun.disparo_jugador()
-    if break_point != 0:
+    if var.break_point != 0:
         print(f'¡Te veo a la próxima, {id_jugador}!')
         break
-    if hay_ganadore != 0:
+    if var.hay_ganadore != 0:
         fun.mostrar_tableros()
         break
     fun.mostrar_tableros()
     fun.disparo_maquina()
-    if hay_ganadore != 0:
+    if var.hay_ganadore != 0:
         fun.mostrar_tableros()
         break
